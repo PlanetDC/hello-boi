8b8b8ac849cfb931c6822f87a3b77d1bdc5bbe7f
5im17andecoi <5im17andecoi@gibwintra.ch>
2018-12-19 16:39:31 +0100

    featureB - new constructor

--- HelloBoi.java
+++ HelloBoi.java
@@ -8,4 +8,15 @@ public class Helloboi {
 		System.out.println(""+text);
 		
 	}
+	
+	public String name;
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
 }
